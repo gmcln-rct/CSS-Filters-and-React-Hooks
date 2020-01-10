7b0a827ef8dbf488708e9e970df65678417a83da
Glenn McClanan <producersama@gmail.com>
2020-01-10 18:13:03 -0500

    Add Context

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
 import React from 'react';
+
+// COMPONENTS
 import BookList from './components/BookList';
 import Navbar from './components/Navbar';
+
+// CONTEXT
+import ThemeContextProvider from './contexts/ThemeContext';
 
 function App() {
   return (
     <div className="App">
-      <Navbar />
-      <BookList />
+      <ThemeContextProvider>
+        <Navbar />
+        <BookList />
+      </ThemeContextProvider>
     </div>
   );
 }
--- src/components/BookList.js
+++ src/components/BookList.js
@@ -1,13 +1,17 @@
 import React, { Component } from 'react';
+import { ThemeContext } from '../contexts/ThemeContext';
 
 class BookList extends Component {
+    static contextType = ThemeContext;
     render() {
+        const { isLightTheme, light, dark } = this.context;
+        const theme = isLightTheme ? light : dark;
         return (
-            <div className="book-list">
+            <div className="book-list" style={{ color: theme.syntax, background: theme.bg }}>
                 <ul>
-                    <li>Eloquent JavaScript</li>
-                    <li>Cracking the Coding Interview</li>
-                    <li>Advanced Algorithms for Dummies</li>
+                    <li style={{ background: theme.ui }}>Eloquent JavaScript</li>
+                    <li style={{ background: theme.ui }}>Cracking the Coding Interview</li>
+                    <li style={{ background: theme.ui }}>Nuclear Fusion for Dummies</li>
                 </ul>
             </div>
         );
