b90007f7ffd5d0ee91a498051d8adfe3ec9c305e
Glenn McClanan <producersama@gmail.com>
2020-01-30 18:01:45 -0500

    Update margins

--- src/components/MovieDetails.js
+++ src/components/MovieDetails.js
@@ -5,11 +5,13 @@ const MovieDetails = ({ Movie }) => {
   const { removeMovie } = useContext(MovieContext);
   return (
     <li onClick={() => removeMovie(Movie.id)}>
-      <div className="title">Title: {Movie.title}</div>
+      <div className="title">Title: <span className='movie-title'>{Movie.title}</span>    </div>
       <div className="director">Director: {Movie.director}</div>
       <div className="year">Year: {Movie.year}</div>
     </li>
   );
 }
+
+
 
 export default MovieDetails;
