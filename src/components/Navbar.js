7349327c64cf262871827f17e09cadb9c8b612bf
Glenn McClanan <producersama@gmail.com>
2020-02-01 16:22:22 -0500

    Restyle navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,9 +1,13 @@
-import React from 'react';
+import React, { useContext } from 'react';
+
+import { ThemeContext } from "../contexts/ThemeContext";
 
 const Navbar = () => {
+
+  const { filterTheme } = useContext(ThemeContext);
   return (
-    <div className="navbar">
-      <h1>Movies To Watch</h1>
+    <div className="navbar" style={{ filter: filterTheme }}>
+      <h1>Move Watchlist</h1>
     </div>
   );
 }
