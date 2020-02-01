d8265e81453a7d0571b1695c43921b537607d9c0
Glenn McClanan <producersama@gmail.com>
2020-02-01 17:18:55 -0500

    Work on dropdown

--- src/components/ThemeSelect.js
+++ src/components/ThemeSelect.js
@@ -1,19 +1,21 @@
 import React, { useContext, useState } from "react";
 
 import { ThemeContext } from "../contexts/ThemeContext";
-
+  let themeName;
 const ThemeSelect = () => {
-  const {   chooseTheme } = useContext(ThemeContext);
+  const {  chooseTheme } = useContext(ThemeContext);
 
   const [filterTheme] = useState('');
 
+
   function handleChange(e) {
     chooseTheme(e.target.value);
+    const { options } = e.target;
+    console.log(options);
   };
 
   function handleSubmit(e) {
     e.preventDefault();
-    // chooseTheme(e.target.value);
   };
 
 
@@ -21,23 +23,25 @@ const ThemeSelect = () => {
       <div className="theme-form">
         <form onSubmit={handleSubmit}>
           <h1>CSS Filters</h1>
-          <h2>Explore how different filters affect the page.</h2>
           <label>
+            Choose from dropdown to explore how different CSS filters affect
+            page elements.
             <select value={filterTheme} onChange={handleChange}>
-                <option>Choose Filter</option>
-                <option value="blur(10px)">Blur</option>
-                <option value="brightness(3)">Brightness</option>
-                <option value="contrast(3)">Contrast</option>
-                <option value="drop-shadow(16px 16px 10px rgba(0,0,0,0.9)">Drop Shadow</option>
-
-
-                <option value="grayscale(100%)">GrayScale</option>
-                <option value="invert(100%)">Invert</option>
-                <option value="sepia(100%)">Sepia</option>
-                <option value="saturate(0%)">Saturate/Desaturate</option>
+              <option>Choose Filter</option>
+              <option value="blur(10px)">Blur</option>
+              <option value="brightness(3)">Brightness</option>
+              <option value="contrast(3)">Contrast</option>
+              <option value="drop-shadow(16px 16px 10px rgba(0,0,0,0.9)">
+                Drop Shadow
+              </option>
+              <option value="grayscale(100%)">GrayScale</option>
+              <option value="invert(100%)">Invert</option>
+              <option value="sepia(100%)">Sepia</option>
+              <option value="saturate(0%)">Saturate/Desaturate</option>
               <option value="none">Clear Filter</option>
             </select>
           </label>
+            <div>Current Filter: {ThemeContext.filterTheme}</div>
           {/* <input type="submit" value="Choose Filter" /> */}
         </form>
       </div>
