afb5d1c83aa33a971d72165721f6f49110eb6bae
Glenn McClanan <producersama@gmail.com>
2020-02-17 11:57:50 -0500

    Streamline top of page

--- src/contexts/MovieContext.js
+++ src/contexts/MovieContext.js
@@ -8,8 +8,7 @@ const MovieContextProvider = (props) => {
     { title: "Citizen Kane", director: "Orson Welles", year: "1941", id: 1 },
     { title: "The Waterboy", director: "Frank Coraci", year: "1998", id: 2 },
     { title: "Remains of the Day", director: "Frank Coraci", year: "1989", id: 3 },
-    { title: "Moonlight", director: "Barry Jenkins", year: "2017", id: 4 },
-    { title: "Harakiri", director: "Masaki Kobayashi", year: "1962", id: 5 }
+
   ]);
 
   const addMovie = (title, director, year) => {
